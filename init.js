b84a10c56b0360b1a0b4f5ecb04603ac99a93557
faizal.a <faizal.a@inapp.com>
2017-03-02 17:15:26 +0530

    Firebase With Firebase UI

---
+++ init.js
@@ -0,0 +1,8 @@
+var config = {
+    apiKey: "AIzaSyBEJ80mWen0fEstvUIFAbH1__l4hzn7zp4",
+    authDomain: "chatapp-4bfce.firebaseapp.com",
+    databaseURL: "https://chatapp-4bfce.firebaseio.com",
+    storageBucket: "chatapp-4bfce.appspot.com",
+    messagingSenderId: "641040744150"
+};
+firebase.initializeApp(config);
